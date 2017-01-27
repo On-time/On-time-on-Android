ec65c6e50f29c13aa198a9e0da828d66fc204c9c
team28079 <team700@hotmail.com>
2017-01-27 11:10:54 +0700

    new class for table

---
+++ app/src/main/java/net/aliveplex/alive/on_timeonandroid/useContentProvider.java
@@ -0,0 +1,74 @@
+package net.aliveplex.alive.on_timeonandroid;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by snail on 1/24/2017.
+ */
+
+public class useContentProvider extends ContentProvider{
+    useSQLiteOpenHelper myDB;
+    SQLiteDatabase db;
+
+    static UriMatcher matcher;
+    {
+        matcher = new UriMatcher((UriMatcher.NO_MATCH));
+        matcher.addURI("mydatabase","PRODUCTS",1);
+        matcher.addURI("mydatabase","SALES",2);
+        matcher.addURI("mydatabase","SALES_PRODUCT",3);
+    }
+    @Override
+    public boolean onCreate() {
+        myDB = new useSQLiteOpenHelper(getContext(),"teamDatabase.db",null,1);
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionAregs, String sortOrder) {
+        Cursor c = null;
+        int i;
+        db = myDB.getReadableDatabase();
+        i = matcher.match(uri);
+        c = db.query("SUBJECT",projection,selection,selectionAregs,null,null,sortOrder);
+
+        return c;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        Uri u = null;
+        long id=0;
+        int i;
+        i = matcher.match(uri);
+        db = myDB.getWritableDatabase();
+        id = db.insert("SUBJECT",null,values);
+        u = ContentUris.withAppendedId(uri,id);
+
+        return u;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+}
---
+++ app/src/main/java/net/aliveplex/alive/on_timeonandroid/useSQLiteOpenHelper.java
@@ -0,0 +1,28 @@
+package net.aliveplex.alive.on_timeonandroid;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by snail on 1/24/2017.
+ */
+
+public class useSQLiteOpenHelper extends SQLiteOpenHelper{
+    public useSQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL = "CREATE TABLE SUBJECT(ID INTEGER PRIMARY KEY,SUBJECTNAME TEXT)";
+        db.execSQL(SQL);
+        SQL = "CREATE TABLE STUDENT(ID INTEGER PRIMARY KEY,ANDROID_ID INTEGER)";
+        db.execSQL(SQL);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
